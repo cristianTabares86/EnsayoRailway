8c76a68fa29b798191512ac340596973954368bb
cristianTabares86 <cristiantabares.iq@gmail.com>
2023-05-10 10:39:18 -0500

    3 commit

--- api/src/db.js
+++ api/src/db.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
 
   //"postgres://postgres:noesmentira1986@localhost/gameshop",
   //"postgres://dbgameshop:3OvsB5VnfrwB9jPRfc2MtJrXnK9OlKJm@dpg-chb3h0ak728tp9bf0e00-a.oregon-postgres.render.com/gameshop?ssl=true",
-  "postgresql://postgres:fWMa6GylLS8iVWHLqbTK@containers-us-west-2.railway.app:6640/gameshop",
+  "postgresql://postgres:fWMa6GylLS8iVWHLqbTK@containers-us-west-2.railway.app:6640/railway",
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
